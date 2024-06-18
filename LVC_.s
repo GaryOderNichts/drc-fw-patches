6e1bd3b4689db9fc147db29df0fec2067fab8839
GaryOderNichts <12049776+GaryOderNichts@users.noreply.github.com>
2024-06-19 00:35:03 +0200

    Add option to add board config to UIC config

--- LVC_.s
+++ LVC_.s
@@ -4,6 +4,7 @@
 
 ; Patches configuration
 ADD_UIC_CONFIG_REGION equ 1
+ADD_UIC_CONFIG_BOARD_CONFIG equ 1
 
 .if ADD_UIC_CONFIG_REGION
 .notice "Adding region to UIC config"
@@ -16,6 +17,19 @@ ADD_UIC_CONFIG_REGION equ 1
 .org 0x000b28d0
     .d32 0x3   ; size
     .d32 0x103 ; eeprom offset
+.endif
+
+.if ADD_UIC_CONFIG_BOARD_CONFIG
+.notice "Adding board config to UIC config"
+
+; Patch jumptable to make ID 4 valid
+.org 0x0003c9c7
+    .d8 0x30
+
+; Patch config table at offset 4 to insert board config
+.org 0x000b28e8
+    .d32 0x3   ; size
+    .d32 0x106 ; eeprom offset
 .endif
 
 .close
